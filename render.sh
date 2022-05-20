d9fb10bca9cd3268c8953507110a7a10421b1a07
Benjamin Reed <git@raccoonfink.com>
2022-05-20 14:21:10 -0400

    get rid of vestigial weasyprint, change output, rsync

--- render.sh
+++ render.sh
@@ -4,26 +4,22 @@ set -e
 
 TOPDIR="$(_dir="$(dirname "$0")"; cd "${_dir}"; echo $PWD)"
 
-export VENV="$HOME/opt/weasyprint"
 export CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
-if [ -e "${VENV}/bin/activate" ] && [ -x "${VENV}/bin/weasyprint" ]; then
-	source "${VENV}/bin/activate"
-fi
 
 cd "${TOPDIR}"
 rm -f .dist/*
-npm run hackmyresume -- build resume.json TO ./dist/resume.html -t theme
+npm run hackmyresume -- build resume.json TO ./dist/benjamin-reed-resume.html ./dist/benjamin-reed-resume.json -t theme
 "$CHROME" --headless \
+	--verbose \
 	--disable-gpu \
 	--run-all-compositor-stages-before-draw \
+	--print-to-pdf=./dist/benjamin-reed-resume.pdf \
 	--print-to-pdf-no-header \
 	--print-to-pdf-print-background \
 	--print-to-pdf-margin-top=0 \
 	--print-to-pdf-margin-bottom=0 \
 	--print-to-pdf-margin-left=0 \
 	--print-to-pdf-margin-right=0 \
-	--print-to-pdf=./dist/resume.pdf \
-	./dist/resume.html
+	./dist/benjamin-reed-resume.html
 
-
+rsync -avr --delete ./dist/ ranger@secure.befunk.com:~/public_html/temp/resume-2022/
